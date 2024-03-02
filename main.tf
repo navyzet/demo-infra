e762041e0d7a1b69309a52342ed20cc91a906598
Nikita Lipchik <nikita.lipchik@exness.com>
2024-03-02 08:17:36 +0200

    Update aws provider

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
+      source = "hashicorp/aws"
+      version = "~> 5.39.0"
     }
   }
 }
@@ -13,20 +13,20 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo" {
-  ami           = "ami-05e451e12993709b7"
-  count = "${var.instance_count}"
-  instance_type = "t2.small"
-  key_name = "Job key"
-  subnet_id = aws_subnet.demo_subnet.id
+  ami                    = "ami-00381a880aa48c6c6"
+  count                  = var.instance_count
+  instance_type          = "t3.small"
+  key_name               = "Job key"
+  subnet_id              = aws_subnet.demo_subnet.id
   vpc_security_group_ids = [aws_security_group.demo_security_group.id]
-  tags          = {
+  tags = {
     Name        = "Demo server ${count.index + 1}"
     Environment = "Demo"
   }
 }
 
 module "key_pair_external" {
-  source = "terraform-aws-modules/key-pair/aws"
+  source     = "terraform-aws-modules/key-pair/aws"
   key_name   = "Job key"
   public_key = file("${path.module}/id_rsa.pub")
 }
